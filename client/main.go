c0e89a3cd439f2f577d481e0ac16ea25b39d02d2
Jeremy Panjaitan <jeremypanjaitan@gmail.com>
2022-04-27 11:07:59 +0700

    [FEATURE] grpc client

---
+++ client/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	pb "gprc-go/grpcgo"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	addr = "localhost:50051"
+)
+
+func main() {
+
+	//try to connect to grpc
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect : %v", err)
+	}
+	defer conn.Close()
+
+	//Create grpc client using the connection that has been established
+	c := pb.NewUserClient(conn)
+
+	//create timeout
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	//call the server function
+	r, err := c.CreateUser(ctx, &pb.UserDataRequest{
+		Name:    "Jeremy Panjaitan",
+		Address: "Jakarta",
+		Age:     22,
+	})
+	if err != nil {
+		log.Fatalf("could not create user : %v", err)
+	}
+
+	log.Printf("result: %s", r.GetMessage())
+}
