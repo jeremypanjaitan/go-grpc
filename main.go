f85067082db4e29de5b3017f4daded9c72efc4b5
Jeremy Panjaitan <jeremypanjaitan@gmail.com>
2022-04-27 10:28:36 +0700

    [FEATURE] create user functionality

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	pb "gprc-go/grpcgo"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedUserServer
+}
+
+func (s *server) CreateUser(ctx context.Context, in *pb.UserDataRequest) (*pb.UserCreatedReply, error) {
+	log.Printf("Received: %v\n", in.GetName())
+	return &pb.UserCreatedReply{Message: "user with name " + in.GetName() + " created", Data: in}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Printf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterUserServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server : %v", err)
+	}
+}
